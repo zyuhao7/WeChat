4872a09aa0027f40a44a81c9a21796f69f2cf02c
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-04-17 22:52:27 +0800

    main

---
+++ ChatClient/main.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QFile>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QFile qss(":/style/stylesheet.qss");
+    if(qss.open(QFile::ReadOnly))
+    {
+        qDebug("open success !");
+        QString style = QLatin1String(qss.readAll());
+        a.setStyleSheet(style);
+        qss.close();
+    }
+    else
+    {
+        qDebug("Open Failed!");
+    }
+
+    QString fileName = "config.ini";
+    QString app_path = QCoreApplication::applicationDirPath();
+
+    QString config_path = QDir::toNativeSeparators(app_path +
+                            QDir::separator() + fileName);
+
+    QSettings settings(config_path, QSettings::IniFormat);
+    QString gate_host = settings.value("GateServer/host").toString();
+    QString gate_port = settings.value("GateServer/port").toString();
+    gate_url_prefix = "http://"+gate_host+":"+ gate_port;
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
