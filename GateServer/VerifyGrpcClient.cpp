f0f2fd4d47dfc138bcffb3f3b540f8f0c6ed3bba
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-04-30 14:20:04 +0800

    VerifyGrpcClient.h/cpp

---
+++ GateServer/VerifyGrpcClient.cpp
@@ -0,0 +1,9 @@
+#include "VerifyGrpcClient.h"
+#include "const.h"
+
+VerifyGrpcClient::VerifyGrpcClient() {
+	auto& gCfgMgr = ConfigMgr::Inst();
+	std::string host = gCfgMgr["VerifyServer"]["Host"];
+	std::string port = gCfgMgr["VerifyServer"]["Port"];
+	pool_.reset(new RPConPool(5, host, port));
+}
---
+++ GateServer/VerifyGrpcClient.h
@@ -0,0 +1,108 @@
+#pragma once
+#include<iostream>
+#include<memory>
+#include<string>
+#include <grpcpp/grpcpp.h>
+#include "message.grpc.pb.h"
+#include "const.h"
+#include "Singleton.h"
+#include "ConfigMgr.h"
+using grpc::Channel;
+using grpc::Status;
+using grpc::ClientContext;
+
+using message::GetVerifyReq;
+using message::GetVerifyRsp;
+using message::VerifyService;
+
+class RPConPool {
+public:
+	RPConPool(size_t poolSize, std::string host, std::string port)
+		: poolSize_(poolSize), host_(host), port_(port), b_stop_(false) {
+		for (size_t i = 0; i < poolSize_; ++i) {
+
+			std::shared_ptr<Channel> channel = grpc::CreateChannel(host + ":" + port,
+				grpc::InsecureChannelCredentials());
+
+			connections_.push(VerifyService::NewStub(channel));
+		}
+	}
+
+	~RPConPool() {
+		std::lock_guard<std::mutex> lock(mutex_);
+		Close();	
+		while (!connections_.empty()) {
+			connections_.pop();
+		}
+	}
+
+	std::unique_ptr<VerifyService::Stub> getConnection() {
+		std::unique_lock<std::mutex> lock(mutex_);
+		cond_.wait(lock, [this] {
+			if (b_stop_) {
+				return true;
+			}
+			return !connections_.empty();
+			});
+		//如果停止则直接返回空指针
+		if (b_stop_) {
+			return  nullptr;
+		}
+		auto context = std::move(connections_.front());
+		connections_.pop();
+		return context;
+	}
+
+	void returnConnection(std::unique_ptr<VerifyService::Stub> context) {
+		std::lock_guard<std::mutex> lock(mutex_);
+		if (b_stop_) {
+			return;
+		}
+		connections_.push(std::move(context));
+		cond_.notify_one();
+	}
+
+	void Close() {
+		b_stop_ = true;
+		cond_.notify_all();
+	}
+
+private:
+	std::atomic<bool> b_stop_;
+	size_t poolSize_;
+	std::string host_;
+	std::string port_;
+	std::queue<std::unique_ptr<VerifyService::Stub>> connections_;
+	std::mutex mutex_;
+	std::condition_variable cond_;
+};
+
+class VerifyGrpcClient :public Singleton<VerifyGrpcClient>
+{
+	friend class Singleton<VerifyGrpcClient>;
+public:
+
+	GetVerifyRsp GetVerifyCode(std::string email) {
+		ClientContext context;
+		GetVerifyRsp reply;
+		GetVerifyReq request;
+		request.set_email(email);
+		auto stub = pool_->getConnection();
+		Status status = stub->GetVerifyCode(&context, request, &reply);
+
+		if (status.ok()) {
+			pool_->returnConnection(std::move(stub));
+			return reply;
+		}
+		else {
+			pool_->returnConnection(std::move(stub));
+			reply.set_error(ErrorCodes::RPCFailed);
+			return reply;
+		}
+	}
+
+private:
+	VerifyGrpcClient();
+
+	std::unique_ptr<RPConPool> pool_;
+};
