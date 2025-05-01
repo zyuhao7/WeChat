dd99f4ada48215faed590879023f94c15d442733
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-05-01 10:13:04 +0800

    MysqlDao.h/cpp

--- StatusServer/MysqlDao.cpp
+++ StatusServer/MysqlDao.cpp
@@ -16,8 +16,6 @@ MysqlDao::~MysqlDao() {
 	pool_->Close();
 }
 
-
-
 int MysqlDao::RegUser(const std::string& name, const std::string& email, const std::string& pwd)
 {
 	auto con = pool_->getConnection();
