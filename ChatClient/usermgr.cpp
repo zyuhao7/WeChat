ebcbe8d2bcf5698ee38146ab4020ffcee95aba9d
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-05-05 15:43:10 +0800

    usermgr.h/cpp

---
+++ ChatClient/usermgr.cpp
@@ -0,0 +1,268 @@
+#include "usermgr.h"
+#include <QJsonArray>
+#include "tcpmgr.h"
+
+UserMgr::~UserMgr()
+{
+
+}
+
+void UserMgr::SetUserInfo(std::shared_ptr<UserInfo> user_info) {
+    _user_info = user_info;
+}
+
+void UserMgr::SetToken(QString token)
+{
+    _token = token;
+}
+
+int UserMgr::GetUid()
+{
+    return _user_info->_uid;
+}
+
+QString UserMgr::GetName()
+{
+    return _user_info->_name;
+}
+
+QString UserMgr::GetIcon()
+{
+    return _user_info->_icon;
+}
+
+std::shared_ptr<UserInfo> UserMgr::GetUserInfo()
+{
+    return _user_info;
+}
+
+// 处理好友申请列表数据
+void UserMgr::AppendApplyList(QJsonArray array)
+{
+    // 遍历 QJsonArray 并输出每个元素
+    for (const QJsonValue &value : array) {
+        auto name = value["name"].toString();
+        auto desc = value["desc"].toString();
+        auto icon = value["icon"].toString();
+        auto nick = value["nick"].toString();
+        auto sex = value["sex"].toInt();
+        auto uid = value["uid"].toInt();
+        auto status = value["status"].toInt();
+        auto info = std::make_shared<ApplyInfo>(uid, name,
+                           desc, icon, nick, sex, status);
+        _apply_list.push_back(info);
+    }
+}
+
+// 处理好友列表数据
+void UserMgr::AppendFriendList(QJsonArray array) {
+    // 遍历 QJsonArray 并输出每个元素
+    for (const QJsonValue& value : array) {
+        auto name = value["name"].toString();
+        auto desc = value["desc"].toString();
+        auto icon = value["icon"].toString();
+        auto nick = value["nick"].toString();
+        auto sex = value["sex"].toInt();
+        auto uid = value["uid"].toInt();
+        auto back = value["back"].toString();
+
+        auto info = std::make_shared<FriendInfo>(uid, name,
+            nick, icon, sex, desc, back);
+
+        // Fixed 避免重复添加相同好友.
+        if (!_friend_map.contains(uid))
+        {
+            _friend_list.push_back(info);
+            _friend_map.insert(uid, info);
+        }
+    }
+}
+
+std::vector<std::shared_ptr<ApplyInfo> > UserMgr::GetApplyList()
+{
+    return _apply_list;
+}
+
+// 添加新的好友申请
+void UserMgr::AddApplyList(std::shared_ptr<ApplyInfo> app)
+{
+    _apply_list.push_back(app);
+}
+
+bool UserMgr::AlreadyApply(int uid)
+{
+    for(auto& apply: _apply_list){
+        if(apply->_uid == uid){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// 分页获取好友聊天列表
+ std::vector<std::shared_ptr<FriendInfo>> UserMgr::GetChatListPerPage() {
+    
+    std::vector<std::shared_ptr<FriendInfo>> friend_list;
+    unsigned long long begin = _chat_loaded;
+    unsigned long long end = begin + CHAT_COUNT_PER_PAGE;
+
+    // 没有更多好友可加载直接返回.
+    if (begin >= _friend_list.size()) {
+        return friend_list;
+    }
+
+    // 说明剩余好友数量不足 CHAT_COUNT_PER_PAGE
+    if (end > _friend_list.size()) {
+        friend_list = std::vector<std::shared_ptr<FriendInfo>>(_friend_list.begin() + begin, _friend_list.end());
+        return friend_list;
+    }
+
+    // 正常分页
+    friend_list = std::vector<std::shared_ptr<FriendInfo>>(_friend_list.begin() + begin, _friend_list.begin()+ end);
+    return friend_list;
+}
+
+// 分页获取联系人列表
+std::vector<std::shared_ptr<FriendInfo>> UserMgr::GetConListPerPage() {
+    std::vector<std::shared_ptr<FriendInfo>> friend_list;
+    unsigned long long begin = _contact_loaded;
+    unsigned long long end = begin + CHAT_COUNT_PER_PAGE;
+
+    if (begin >= _friend_list.size()) {
+        return friend_list;
+    }
+
+    if (end > _friend_list.size()) {
+        friend_list = std::vector<std::shared_ptr<FriendInfo>>(_friend_list.begin() + begin, _friend_list.end());
+        return friend_list;
+    }
+
+
+    friend_list = std::vector<std::shared_ptr<FriendInfo>>(_friend_list.begin() + begin, _friend_list.begin() + end);
+    return friend_list;
+}
+
+
+UserMgr::UserMgr():_user_info(nullptr), _chat_loaded(0),_contact_loaded(0)
+{
+
+}
+
+// 当收到服务器返回的好友申请处理结果时触发
+void UserMgr::SlotAddFriendRsp(std::shared_ptr<AuthRsp> rsp)
+{
+    AddFriend(rsp);
+}
+
+// 处理好友验证信息 当我同意对方的好友请求后，服务器通知我添加用户信息条目
+void UserMgr::SlotAddFriendAuth(std::shared_ptr<AuthInfo> auth)
+{
+    AddFriend(auth);
+}
+
+bool UserMgr::IsLoadChatFin() {
+    if ((unsigned long long)_chat_loaded >= _friend_list.size()) {
+        return true;
+    }
+
+    return false;
+}
+
+void UserMgr::UpdateChatLoadedCount() {
+    unsigned long long begin = _chat_loaded;
+    unsigned long long end = begin + CHAT_COUNT_PER_PAGE;
+
+    if (begin >= _friend_list.size()) {
+        return ;
+    }
+
+    if (end > _friend_list.size()) {
+        _chat_loaded = _friend_list.size();
+        return ;
+    }
+
+    _chat_loaded = end;
+}
+
+void UserMgr::UpdateContactLoadedCount() {
+    unsigned long long begin = _contact_loaded;
+    unsigned long long end = begin + CHAT_COUNT_PER_PAGE;
+
+    if (begin >= _friend_list.size()) {
+        return;
+    }
+
+    if (end > _friend_list.size()) {
+        _contact_loaded = _friend_list.size();
+        return;
+    }
+
+    _contact_loaded = end;
+}
+
+bool UserMgr::IsLoadConFin()
+{
+    if ((unsigned long long)_contact_loaded >= _friend_list.size()) {
+        return true;
+    }
+
+    return false;
+}
+
+bool UserMgr::CheckFriendById(int uid)
+{
+    auto it = _friend_map.find(uid);
+    if(it == _friend_map.end()){
+        return false;
+    }
+
+    return true;
+}
+
+void UserMgr::AddFriend(std::shared_ptr<AuthRsp> auth_rsp)
+{
+    // Fixed
+    auto friend_info = std::make_shared<FriendInfo>(auth_rsp);
+    if (_friend_map.contains(friend_info->_uid))
+            return;
+
+    _friend_list.push_back(friend_info);
+    _friend_map[friend_info->_uid] = friend_info;
+}
+
+void UserMgr::AddFriend(std::shared_ptr<AuthInfo> auth_info)
+{
+    //Fixed
+    auto friend_info = std::make_shared<FriendInfo>(auth_info);
+    if (_friend_map.contains(friend_info->_uid))
+            return;
+
+    _friend_list.push_back(friend_info);
+    _friend_map[friend_info->_uid] = friend_info;
+}
+
+std::shared_ptr<FriendInfo> UserMgr::GetFriendById(int uid)
+{
+    auto find_it = _friend_map.find(uid);
+    if(find_it == _friend_map.end()){
+        return nullptr;
+    }
+
+    return *find_it;
+}
+
+
+
+void UserMgr::AppendFriendChatMsg(int friend_id, std::vector<std::shared_ptr<TextChatData> > msgs)
+{
+    auto find_iter = _friend_map.find(friend_id);
+    if(find_iter == _friend_map.end()){
+        qDebug()<<"append friend uid  " << friend_id << " not found";
+        return;
+    }
+
+    find_iter.value()->AppendChatMsgs(msgs);
+}
+
+
