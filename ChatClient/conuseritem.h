54bfaa870d2d057bcb6eb85d8d1b8156344c53b9
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-05-13 23:45:18 +0800

    conuseritem.h/cpp

---
+++ ChatClient/conuseritem.h
@@ -0,0 +1,31 @@
+#ifndef CONUSERITEM_H
+#define CONUSERITEM_H
+
+#include <QWidget>
+#include "listitembase.h"
+#include "userdata.h"
+
+namespace Ui {
+class ConUserItem;
+}
+
+class ConUserItem : public ListItemBase
+{
+    Q_OBJECT
+
+public:
+    explicit ConUserItem(QWidget *parent = nullptr);
+    ~ConUserItem();
+    QSize sizeHint() const override;
+    void SetInfo(std::shared_ptr<AuthInfo> auto_info);
+    void SetInfo(std::shared_ptr<AuthRsp> auth_rsp);
+    void SetInfo(int uid, QString name, QString icon);
+    void ShowRedPoint(bool show = false);
+    std::shared_ptr<UserInfo> GetInfo();
+
+private:
+    Ui::ConUserItem *ui;
+    std::shared_ptr<UserInfo> _info;
+};
+
+#endif // CONUSERITEM_H
