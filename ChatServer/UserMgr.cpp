7ed6447796f44627de7470b5918e0e4c3076d3ea
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2025-08-03 21:29:04 +0800

    UserMgr.h/cpp

---
+++ ChatServer/UserMgr.cpp
@@ -0,0 +1,38 @@
+#include "UserMgr.h"
+#include "CSession.h"
+#include "RedisMgr.h"
+
+UserMgr::~UserMgr()
+{
+	_uid_to_session.clear();
+}
+
+std::shared_ptr<CSession> UserMgr::GetSession(int uid)
+{
+	std::lock_guard<std::mutex> lock(_session_mtx);
+	auto it = _uid_to_session.find(uid);
+	if (it == _uid_to_session.end())
+		return nullptr;
+
+	return it->second;
+}
+
+void UserMgr::SetUserSession(int uid, std::shared_ptr<CSession> session)
+{
+	std::lock_guard<std::mutex> lock(_session_mtx);
+	_uid_to_session[uid] = session;
+}
+
+void UserMgr::RmvUserSession(int uid)
+{
+	auto uid_str = std::to_string(uid);
+	RedisMgr::GetInstance()->Del(USERIPPREFIX + uid_str);
+
+	{
+		std::lock_guard<std::mutex> lock(_session_mtx);
+		_uid_to_session.erase(uid);
+	}
+}
+
+UserMgr::UserMgr()
+{}
